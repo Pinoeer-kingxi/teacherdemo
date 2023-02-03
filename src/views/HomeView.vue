3bb05ad216796633ebf48ad8f3d57e1af717b01b
Magic Book <1163631058@qq.com>
2023-02-03 21:12:42 +0800

    上传内容

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,84 @@
   <div id="HomeView">
     <template>
       <div id="building">
-        <div class="box">
-          <a href="http://news.snnu.edu.cn/" target="_blank">模范先锋榜样力量</a>
+        <div class="box" id="left">
+          <a class="title" href="http://news.snnu.edu.cn/" target="_blank">模范先锋榜样力量</a>
+          <ul>
+            <li><a href="https://mp.weixin.qq.com/s/O44meAof-iItEbsqFyxbTA" class="left" target="_blank">红烛闪耀丨我在本溪当老师</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="https://mp.weixin.qq.com/s/My5tLk6t4mfJMw0Em4n4TA" class="left" target="_blank">红烛闪耀 | 我在陵水当老师</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="https://mp.weixin.qq.com/s/tihBqaMV--P69Kafamf9LA" class="left" target="_blank">西部红烛丨我在阳泉当老师</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="https://mp.weixin.qq.com/s/pYf9P8gG8PI5wMjMpvUoZg" class="left" target="_blank">看见“一面墙”  推开“一扇窗”</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="http://www.snnu.edu.cn/info/1012/26590.htm" class="left" target="_blank">导师栗洪武教授的育人之道</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="http://www.snnu.edu.cn/info/1012/26589.htm" class="left" target="_blank">让黔北大地飞出更多“金凤凰”</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="https://mp.weixin.qq.com/s/UfMiwlM5kB6QP_DNsL-zVQ" class="left" target="_blank">杨钰莹：青春立志，追梦未来</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="http://www.snnu.edu.cn/info/1012/25806.htm" class="left" target="_blank">王淑兰：难忘那团结奋进艰苦创业的八年岁月</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="https://mp.weixin.qq.com/s/14hJVxH8u-3IaI-7yUpUuw" class="left" target="_blank">师大国旗护卫队：苦有所为 苦有所乐 为...</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="http://www.snnu.edu.cn/info/1012/25757.htm" class="left" target="_blank">中国科学院院士房喻：潜心四十载 以学术...</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="https://mp.weixin.qq.com/s/aZJavwN-EsoNMOglBJagEQ" class="left" target="_blank">西部红烛 | 一场跨越600公里的教育之旅</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="https://mp.weixin.qq.com/s/_7TJJDlJjBEbuh8qhbcGuA" class="left" target="_blank">西部红烛｜我在格尔木当老师</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="http://www.snnu.edu.cn/info/1012/24575.htm" class="left" target="_blank">我和我的老师 丨 山高水长 红烛闪耀 师...</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="https://mp.weixin.qq.com/s/OMsOYvtWsZ5A1D1NnNLHbg" class="left" target="_blank">西部红烛 | 保持无尽热爱，奔赴人生山海...</a>
+              <div class="clear"></div>
+            </li>
+            <li><a href="http://www.snnu.edu.cn/info/1012/24325.htm" class="left" target="_blank">“传薪者”杨清源：奉献一生光和热 让真...</a>
+              <div class="clear"></div>
+            </li>
+          </ul>
         </div>
-        <div class="box">
-          <a href="http://edu.people.com.cn/" target="_blank">政策热点交流</a>
+        <div class="box" id="middle">
+          <a class="title" href="http://edu.people.com.cn/" target="_blank">政策热点交流</a>
+          <ul>
+            <li><a href='/n1/2023/0202/c1006-32616429.html' target="_blank">高校对校外人员开放了？专家：应逐步适度…</a></li>
+            <li><a href='/n1/2023/0201/c1006-32615722.html' target="_blank">2023春季高校毕业生大型招聘会福州线…</a></li>
+            <li><a href='/n1/2023/0201/c1006-32615659.html' target="_blank">想去三、四线城市就业大学生占比呈上升趋势</a></li>
+            <li><a href='/n1/2023/0201/c1006-32615627.html' target="_blank">9所高校共同成立卓越大学智库联盟</a></li>
+            <li><a href='/n1/2023/0201/c1006-32615625.html' target="_blank">加强管理，确保校级科研机构良性运转</a></li>
+            <li><a href='/n1/2023/0202/c1006-32616355.html' target="_blank">做好准备迎开学</a></li>
+            <li><a href='/n1/2023/0201/c1006-32615726.html' target="_blank">杭州拟调整中考分配生制度 学校不再对学…</a></li>
+            <li><a href='/n1/2023/0201/c1006-32615590.html' target="_blank">精心准备迎开学</a></li>
+            <li><a href='/n1/2023/0131/c1006-32614907.html' target="_blank">异地办学按下“暂停键”，高等教育应如何…</a></li>
+            <li><a href='/n1/2023/0131/c1006-32614900.html' target="_blank">素质教育如何突破“两难纠结”</a></li>
+          </ul>
         </div>
-        <div class="box">
-          <a href="https://www.cnki.net/" target="_blank">知识学术前沿</a>
+        <div class="box" id="right">
+          <a class="title" href="https://www.cnki.net/" target="_blank">知识学术前沿</a>
+          <ul>
+            <li><a href="https://mp.weixin.qq.com/s/ZIVN4WGpoUdp2M3bnIjL7g" target="_blank">校长谈 丨鲁照斌：关于家校合作、统合课程与学习共同体的论述</a></li>
+            <li><a href="https://mp.weixin.qq.com/s/CVVsoBsCi8mbK5QsNlMwLw" target="_blank">双减案例丨课后服务提质的实践探索</a></li>
+            <li><a href="https://mp.weixin.qq.com/s/XRtBUYkf6vVPONrPXLabOg" target="_blank">学   堂丨你想要的专家讲座视频回放，都在这里了！</a></li>
+            <li><a href="https://mp.weixin.qq.com/s/Yx9yq7GvkoDyvKH8NIHfvg" target="_blank">学   堂 | 《比较教育学报》副主编和你谈谈投稿的注意事项</a></li>
+            <li><a href="https://mp.weixin.qq.com/s/a1NBqVRnTtUnTy_7LEUhuw" target="_blank">专家解读 | 如何落实学科核心素养？</a></li>
+            <li><a href="https://mp.weixin.qq.com/s/fizJ2IKvwZmFhOfA_wM9lQ" target="_blank">文献推荐 | 从跨学科到超学科：基于高中生物教学的20年探索</a></li>
+            <li><a href="https://mp.weixin.qq.com/s/HObmZr7Wk0ICIM11QUXYTw" target="_blank">文献推荐丨阅读教学的课堂三“练”</a></li>
+            <li><a href="https://mp.weixin.qq.com/s/nHpmfrdPt3j6hqFRwdajPw" target="_blank">文献推荐丨关于校园文化建设的几点建议</a></li>
+            <li><a href="https://mp.weixin.qq.com/s/0Opa8tUU71htJxMl-dQtJQ" target="_blank">文献推荐丨关于校园文化建设的部分实践</a></li>
+          </ul>
         </div>
       </div>
     </template>
@@ -26,16 +96,29 @@ export default {
 #HomeView{
   padding: 15px;
 }
+
 #building{
   background:url("@/assets/background2.jpg");
   width: 100%;
-  height: 100%;
+  height: 80%;
   position: fixed;
-  background-size: 100% 100%;
+  background-size: 100% 90%;
 }
 
 .box{
-  width: 33.3%;
+  float:left;
+  width:20em;
+  font-size:10pt;
+  color: #545c64;
+  font-family:仿宋;
+  text-decoration: none;
+}
+
+.title{
+  font-size: 15pt;
+  color: #222222;
+  font-family: 宋体 ;
+  text-decoration: none;
 }
 
 </style>
